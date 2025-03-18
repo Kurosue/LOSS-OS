a88586fa23f509ee426a56f214b3a871c5c57881
mlqmn <13523044@std.stei.itb.ac.id>
2025-03-18 23:03:44 +0700

    feat: add text framebuffer driver

---
+++ src/framebuffer.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "header/text/framebuffer.h"
+#include "header/stdlib/string.h"
+#include "header/cpu/portio.h"
+
+void framebuffer_set_cursor(uint8_t r, uint8_t c) {
+    uint16_t pos = r * 80 + c;
+	out(CURSOR_PORT_CMD, 0x0F);
+	out(CURSOR_PORT_DATA, (uint8_t) (pos & 0xFF));
+	out(CURSOR_PORT_CMD, 0x0E);
+	out(CURSOR_PORT_DATA, (uint8_t) ((pos >> 8) & 0xFF));
+
+}
+
+void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
+    FRAMEBUFFER_MEMORY_OFFSET[(row * 80 + col) * 2] = c;
+    FRAMEBUFFER_MEMORY_OFFSET[(row * 80 + col) * 2 + 1] = fg | bg;
+}
+
+void framebuffer_clear(void) {
+    for (int i = 0; i < 80 * 25; i++){
+        FRAMEBUFFER_MEMORY_OFFSET[i*2] = 0x00;
+        FRAMEBUFFER_MEMORY_OFFSET[i*2+1] = 0x07;
+    }
+}
--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include "header/cpu/gdt.h"
 #include "header/kernel-entrypoint.h"
+#include "header/text/framebuffer.h"
 
 void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
